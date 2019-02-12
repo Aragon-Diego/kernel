0cc77e3a0538ec7b045262baf3fe5eebe4d1db32
Diego Aragon <diego.aragonCursos@gmail.com>
2019-02-12 17:49:17 -0600

    se agregó las interrupciones

--- src/containers/interrupciones/Interrupciones.js
+++ src/containers/interrupciones/Interrupciones.js
@@ -10,7 +10,12 @@ const Interrupciones = (props) =>{
             </select>
             <p className="inter1">Interrupcion: </p>
             <select className="select2">
-                <option value="SVC1">SVC cuantum expirado</option>
+                <option value="SVC1">SVC de solicitud de I/O</option>
+                <option value="SVC2">SVC de terminación normal</option>
+                <option value="SVC3">SVC de solicitud de fecha</option>
+                <option value="Error_Porgrama">Error de programa</option>
+                <option value="Quantum_Expirado">Externa de quantum expirado</option>
+                <option value="I/O">Dispositivo I/O</option>
             </select>
         </div>
     );
