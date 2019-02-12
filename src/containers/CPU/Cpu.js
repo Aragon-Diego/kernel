f76a63db6d9d4e40b54498fa43f4073adcd71a9c
Diego Aragon <diego.aragonCursos@gmail.com>
2019-02-12 17:45:26 -0600

    se agregó el bloque de CPU

--- src/containers/CPU/Cpu.js
+++ src/containers/CPU/Cpu.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './Cpu.css';
 import Bloque from './bloque/Bloque';
 const Cpu = (props) =>{
-    let procesoEnRunning=[<p className="relleno">Nombre: {props.proceso.nombre}</p>,
+    let procesoEnRunning=[
+    <p className="relleno">Nombre: {props.proceso.nombre}</p>,
     <p className="relleno">Tpo llegada: {props.proceso.tpo}</p>,
     <p className="relleno">CPU Asignado: {props.proceso.asignado}</p>,
     <p className="relleno">Envejecimiento: {props.proceso.envejecimineto}</p>,
     <p className="relleno">CPU Restante: {props.proceso.restane}</p>,
     <p className="relleno">Quantum restante: {props.proceso.quantum}</p>
     ];
-    let CPU="";
+    let CPU=[
+        <div className="parte1">
+            <select >
+                <option value="FIFO">FIFO</option>
+                <option value="RR">Round Robin</option>
+                <option value="SRT">SRT (apropiativo)</option>
+                <option value="HRRN">HRRN (apropiativo)</option>
+            </select>
+        </div>,
+        <div className="parte2">
+            <p>Tam Quantum:</p>
+            <input></input>
+        </div>
+    ];
     return(
         <div className="divCpu">
             <div className="titulo">
