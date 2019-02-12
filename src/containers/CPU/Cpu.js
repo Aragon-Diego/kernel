15a54ad7d74d028f5d6d5d9429454a9056318541
Diego Aragon <diego.aragonCursos@gmail.com>
2019-02-12 16:36:22 -0600

    se agregó el bloque de scheduling

--- src/containers/CPU/Cpu.js
+++ src/containers/CPU/Cpu.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './Cpu.css';
 import Bloque from './bloque/Bloque';
 const Cpu = (props) =>{
+    let procesoEnRunning=[<p className="relleno">Nombre: {props.proceso.nombre}</p>,
+    <p className="relleno">Tpo llegada: {props.proceso.tpo}</p>,
+    <p className="relleno">CPU Asignado: {props.proceso.asignado}</p>,
+    <p className="relleno">Envejecimiento: {props.proceso.envejecimineto}</p>,
+    <p className="relleno">CPU Restante: {props.proceso.restane}</p>,
+    <p className="relleno">Quantum restante: {props.proceso.quantum}</p>
+    ];
+    let CPU="";
     return(
         <div className="divCpu">
             <div className="titulo">
                 <p>CPU</p>
             </div>
             <div className="bloques">
-                <Bloque titulo="Scheduling"/>
-                <Bloque titulo="CPU"/>
+                <Bloque titulo="Scheduling" relleno={procesoEnRunning}/>
+                <Bloque titulo="CPU" relleno={CPU}/>
             </div>
         </div>
     );
